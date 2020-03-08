6a154c135494ec30b9c3355f9976d945b959ae42
Bill78Zhang <wohencool21@gmail.com>
2020-03-08 20:26:03 +1030

    add new components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './home-page.components';
+import HomePage from './components/pages/homepage/home-page.components';
 
 function App() {
   return (
---
+++ src/components/pages/homepage/home-page.components.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../homepage/homepage.style.scss';
+import Directory from '../../directory/directory.compomemts';
+
+const homePage = () => {
+  return (
+    <div className='homePage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default homePage;
